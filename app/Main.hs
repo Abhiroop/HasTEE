bdc4cb66993b5adb15106fdae53728bf039b3e2a
Abhiroop <asiamgenius@gmail.com>
2022-12-27 22:04:11 +0100

    Move IFC experiments to Program.hs (we need to shift to more modern libraries like mac)

--- app/Main.hs
+++ app/Main.hs
@@ -1,66 +1,17 @@
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-{-# OPTIONS_GHC -Wno-missing-methods #-}
+{-# LANGUAGE StaticPointers #-}
 module Main (main) where
 
-import Data.Char
-import Lib
-import Sec
-import SecIO
+import GHC.StaticPtr
 
-{-@
- Non-interferent; Public output is not influenced
- by private
-@-}
-f1 :: (Char, Int) -> (Char, Int)
-f1 (c, i) = (chr (ord c + i), i + 3)
+fact :: Int -> Int
+fact 0 = 1
+fact n = n * fact (n - 1)
 
-{-@
- Information Leak; Public output is
- influenced by private
-@-}
-f2 :: (Char, Int) -> (Char, Int)
-f2 (c, _) = (c, ord c)
+main = do
+  let sptr :: StaticPtr (Int -> Int)
+      sptr = static fact
+  print $ staticPtrInfo sptr
+  print $ deRefStaticPtr sptr 10
 
-{-@
- Information Leak; Public output is
- influenced by private
-@-}
-f3 :: (Char, Int) -> (Char, Int)
-f3 (c, _) = (c, if ord c > 31 then 1 else 0)
-
-data H = H
-
-f4 :: (Sec H Char, Int) -> (Sec H Char, Int)
-f4 (sc, i) = (fmap ordinc sc, i + 3)
-  where
-    ordinc = \c -> chr (ord c + i)
-
-{-@ The `open` function needs to be restricted
-    in terms of who can open it.
-@-}
-f5 :: (Sec H Char, Int) -> (Sec H Char, Int)
-f5 (sc, _) = (sc, open (fmap ord sc) H)
-
-
-
-file1, file2 :: File s
-file1 = undefined
-file2 = undefined
-writeToAFile :: Sec H String -> Sec H (SecIO H ())
-writeToAFile secs =
-  (\s -> if length s < 10
-         then writeSecIO file1 s
-         else writeSecIO file2 s) `fmap` secs
-
-
-foo = do
-  let secret = undefined :: Sec H String
-  let secStr = readSecIO file1
-  writeToAFile secret
---  let secStr2 = readSecIO file2
-
-
-
-
-main :: IO ()
-main = someFunc
+-- main :: IO ()
+-- main = putStrLn "Hello World"
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,15 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib ( someFunc ) where
+
+{-
+runClient :: Client () -> App Done
+liftServerIO :: IO a -> App (Server a)
+remote :: Remotable a => a -> App a
+onServer :: Remote (Server a) -> Client a
+(<.>) :: Serialize a => Remote (a -> b) -> a -> Remote b
+getSessionID :: Server SessionID
+
+
+-}
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
