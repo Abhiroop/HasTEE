f6e04560e39f099821197d606eaefdf6701c4857
Robert Krook <krookr@chalmers.se>
2023-03-01 19:40:52 +0100

    tidy up

--- app/Main.hs
+++ app/Main.hs
@@ -3,10 +3,8 @@
 module Main where
 
 import Control.Monad.IO.Class(liftIO)
-import System.IO
 import Text.Read ( readMaybe )
 import Data.Binary
-import Data.Word
 import GHC.Generics
 
 
@@ -21,19 +19,9 @@ import Client as API
 -- * Return codes
 
 data ReturnCode
-  = Success
-  | PasswordOutOfRange
-  | WalletAlreadyExists
-  | CannotSaveWallet
-  | CannotLoadWallet
-  | WrongMasterPassword
-  | WalletFull
-  | ItemDoesNotExist
-  | ItemAlreadyExists
-  | ItemTooLong
-  | FailSeal
-  | FailUnseal
-  deriving (Generic, Show)
+  = Success | PasswordOutOfRange | WalletAlreadyExists | CannotSaveWallet | CannotLoadWallet
+  | WrongMasterPassword | WalletFull | ItemDoesNotExist | ItemAlreadyExists | ItemTooLong
+  | FailSeal | FailUnseal deriving (Generic, Show)
 
 instance Binary ReturnCode
 
@@ -123,16 +111,6 @@ addItem mp item username pass
         Just w | itemExists item username (items w) -> return ItemAlreadyExists
         Just w -> saveWallet (w { items = (Item item username pass) : items w, size = size w + 1})
 
-{-@
-addItem :: Password -> String -> String -> Password -> Server ReturnCode
-addItem mp item username pass = do
-    w <- loadWallet
-    case w of
-        Nothing -> return CannotLoadWallet
-        Just w | not (masterPassword w == mp) -> return WrongMasterPassword
-        Just w -> saveWallet (w { items = (Item item username pass) : items w, size = size w + 1})
-@-}
-
 removeItem :: Password -> String -> String -> Server ReturnCode
 removeItem mp title' username' = do
   w <- loadWallet
@@ -161,19 +139,6 @@ showItem mp title' username' = do
       let singleton = filter (\t -> title t == title' && username t == username') items
       in password (head singleton)
 
-{-@
--- | Return a password from the password wallet, given the title and username
-showItem :: Password -> String -> String -> Server (Either ReturnCode Password)
-showItem mp title' username' = do
-  w <- loadWallet
-  case w of
-    Nothing -> return $ Left CannotLoadWallet
-    -- If the supplied master password is wrong, abort
-    Just w | not (masterPassword w == mp) -> return $ Left WrongMasterPassword
-    -- otherwise we look up the password and return it to the client
-    Just w -> return $ Right (findPass title' username' (items w))
-@-}
-
 itemExists :: String -> String -> [Item] -> Bool
 itemExists title' uname items =
   any (\t -> title t == title' && username t == uname) items
@@ -190,39 +155,6 @@ handleCommand cmd = case cmd of
 -- * The application
 
 data Api = Api { execute :: Remote (Command -> Server (Either ReturnCode String)) }
-
-{-@
--- | Commands exposed by the password wallet
-data Command
-    = Create Password
-    -- ^ Create a new wallet with the given master password
-    | Add Password String String Password
-    -- ^ Add an item to the password wallet
-    | Show Password String String
-    -- ^ Retrieve a password from the password wallet
-    -- more commands, omitted for brevity
-  deriving (Show, Binary)
-
--- | Execute a command in the enclave
-handleCommand :: Command -> Server (Either ReturnCode String)
-handleCommand cmd = case cmd of
-  Create s        -> fmap Left $ createWallet s
-  Add s str cs s' -> fmap Left $ addItem s str cs s'
-  Show s str cs   -> showItem s str cs
-  -- more commands, omitted for brevity
-
--- | Main application with the configuration code and client logic
-app :: App Done
-app = do
-    -- make handleCommand callable on the client
-    execute <- remote handleCommand
-    runClient $ do
-      cmd <- getCommand -- parse input arguments
-      r <- onEnclave $ execute <.> cmd
-      case r of
-        Left code -> liftIO $ putStrLn $ show code
-        Right p   -> liftIO $ putStrLn $ concat ["retrieved pass: ", show p]
-@-}
 
 app :: App Done
 app = do
