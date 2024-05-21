2190eb94e34ba043a20279fe32e4dcc165513938
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2024-05-21 22:49:04 +0200

    Simplify example even further.

--- examples/BookSeller.hs
+++ examples/BookSeller.hs
@@ -21,15 +21,20 @@ type Price = Int
 type Day = Int
 type BookStore = Map String (Price, Day)
 storeLookup :: Enclave (Ref BookStore) -> String -> Enclave (Price, Day)
-storeLookup books book =
-    fromJust . Map.lookup book
-        <$> (readRef =<< books)
+storeLookup secBooks book = do
+    booksRef <- secBooks         :: Enclave (Ref BookStore)
+    books    <- readRef booksRef :: Enclave BookStore
+    return . fromJust $ Map.lookup book books
 
 getPrice :: Enclave (Ref BookStore) -> String -> Enclave Price
-getPrice books book = fst <$> storeLookup books book
+getPrice books book = do
+    (price,_ ) <- storeLookup books book
+    return price
 
 getDay :: Enclave (Ref BookStore) -> String -> Enclave Day
-getDay books book = snd <$> storeLookup books book
+getDay books book = do
+    (_, deliveryDate) <- storeLookup books book
+    return deliveryDate
 
 defaultBooks :: BookStore
 defaultBooks =
@@ -38,22 +43,21 @@ defaultBooks =
         , ("Homotopy Type Theory", (120, 20230101))
         ]
 
--- Maybe remove this and let app have a state?
-budget :: Int
-budget = 100
+clientBudget :: Int
+clientBudget = 100
 
 bookseller :: App Done
 bookseller = do
     remoteRef <- liftNewRef defaultBooks        :: App (Enclave (Ref BookStore))
     getPrice  <- inEnclave (getPrice remoteRef) :: App (Secure (String -> Enclave Price))
     getDay    <- inEnclave (getDay remoteRef)   :: App (Secure (String -> Enclave Day))
-    
+
     runClient $ do
         liftIO    $ putStrLn "Enter the title of the book to buy"
         userInput <- liftIO getLine
         price     <- gateway (getPrice <@> userInput) :: Client Price
 
-        if price < budget then do
+        if price < clientBudget then do
             day    <- gateway (getDay <@> userInput) :: Client Day
             liftIO $ putStrLn $ "The book will be delivered on " <> show day
         else do
